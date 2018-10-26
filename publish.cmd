7cb127ecafddb29407326f3ef65ee5cd58c2c477
Dana VanHooser <v-danava@microsoft.com>
2018-10-26 10:42:48 -0700

    Initial upload. Created from Azure portal, downloaded zip package and pushing from laptop.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBotWithCounter.sln -p:DeployOnBuild=true -p:PublishProfile=v-danavaechobot-Web-Deploy.pubxml -p:Password=2Hgs8hrsCD2u8acCvyE5FMKlZgkSlPADMeMlz6fwXt7aDGjj0WbKP0AxqLqL
+
